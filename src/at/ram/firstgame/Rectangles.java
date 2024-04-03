1928b8ba14e116813f20a6aa53c23dff5f6d21c2
Raphael <raphael.maringer@student.htldornbirn.at>
2024-04-03 09:32:13 +0200

    modified 01-firstgame

--- src/at/ram/firstgame/Rectangles.java
+++ src/at/ram/firstgame/Rectangles.java
@@ -6,6 +6,9 @@ public class Rectangles extends BasicGame {
     private float x;
     private float y;
 
+    private float oval_x;
+    private float oval_y;
+
     private float rec_x;
     private float rec_y;
     private float speed;
@@ -22,7 +25,8 @@ public class Rectangles extends BasicGame {
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
-        this.x += (float)delta/this.speed;
+        this.rec_x += (float) delta / this.speed;
+
 
         /* this.x++;
         this.y++;
@@ -30,23 +34,23 @@ public class Rectangles extends BasicGame {
             this.x = 0;
             this.y = 0;
         } */
-    }
-
-    @Override
-    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        graphics.drawRect(this.x, this.y, 100, 100);
-        graphics.drawString("Hello you!", 50, 50);
-        graphics.drawOval(this.);
-    }
-
-    public static void main(String[] argv) {
-        try {
-            AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
-            container.setDisplayMode(800, 600, false);
-            container.start();
-        } catch (SlickException e) {
-            e.printStackTrace();
         }
-    }
 
-}
+        @Override
+        public void render (GameContainer gameContainer, Graphics graphics) throws SlickException {
+            graphics.drawString("Hello you!", 50, 50);
+            graphics.drawRect(this.rec_x, this.rec_y, 100, 100);
+            graphics.drawOval(this.oval_x, this.oval_y, 100, 50);
+        }
+
+        public static void main (String[]argv){
+            try {
+                AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
+                container.setDisplayMode(800, 600, false);
+                container.start();
+            } catch (SlickException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
