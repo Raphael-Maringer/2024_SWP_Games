dfea3d1da848d2f259dbb8322ccda518f4c4930c
Raphael <raphael.maringer@student.htldornbirn.at>
2024-04-17 08:44:18 +0200

    modified 01-firstgame (movement not working)

--- src/at/ram/firstgame/Rectangles.java
+++ src/at/ram/firstgame/Rectangles.java
@@ -3,14 +3,17 @@ package at.ram.firstgame;
 import org.newdawn.slick.*;
 
 public class Rectangles extends BasicGame {
-    private float x;
-    private float y;
+    private float sq_x;
+    private float sq_y;
 
     private float oval_x;
     private float oval_y;
 
     private float rec_x;
     private float rec_y;
+
+    private float cir_x;
+    private float cir_y;
 
     private float speed;
 
@@ -20,15 +23,22 @@ public class Rectangles extends BasicGame {
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-        this.x = 100;
-        this.speed = 50.0f;
+        this.sq_x = 100;
+        this.speed = 2.0f;
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
+        this.rec_x = 50;
+        this.rec_y = 50;
         this.rec_x += (float) delta / this.speed;
-        this.oval_x += delta / this.speed;
 
+        if (this.oval_x < 500) {
+            this.oval_x += (float) delta / this.speed;
+        } else if (this.oval_x > 500) {
+            this.oval_x += (float) delta / this.speed; }
+
+        this.cir_y += (float) delta / this.speed;
 
         /* this.x++;
         this.y++;
@@ -36,23 +46,24 @@ public class Rectangles extends BasicGame {
             this.x = 0;
             this.y = 0;
         } */
-        }
-
-        @Override
-        public void render (GameContainer gameContainer, Graphics graphics) throws SlickException {
-            graphics.drawString("Hello you!", 50, 50);
-            graphics.drawRect(this.rec_x, this.rec_y, 100, 100);
-            graphics.drawOval(this.oval_x, this.oval_y, 100, 50);
-        }
-
-        public static void main (String[]argv){
-            try {
-                AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
-                container.setDisplayMode(800, 600, false);
-                container.start();
-            } catch (SlickException e) {
-                e.printStackTrace();
-            }
-        }
-
     }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        graphics.drawString("Hello you!", 50, 50);
+        graphics.drawRect(this.rec_x, this.rec_y, 100, 100);
+        graphics.drawOval(this.oval_x, this.oval_y, 100, 50);
+        graphics.drawOval(this.cir_x, this.cir_y, 100, 100);
+    }
+
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
+            container.setDisplayMode(800, 600, false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
