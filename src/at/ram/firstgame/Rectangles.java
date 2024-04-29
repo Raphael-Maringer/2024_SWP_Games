bbcfe7209509c507c4bec7145b27fb83f23c7c1c
Raphael <raphael.maringer@student.htldornbirn.at>
2024-04-29 14:49:04 +0200

    modified 01-firstgame

--- src/at/ram/firstgame/Rectangles.java
+++ src/at/ram/firstgame/Rectangles.java
@@ -3,17 +3,19 @@ package at.ram.firstgame;
 import org.newdawn.slick.*;
 
 public class Rectangles extends BasicGame {
+    private enum DIRECTION{RIGHT, LEFT, DOWN, UP};
+
     private float sq_x;
     private float sq_y;
+    private DIRECTION sq_direction = DIRECTION.RIGHT;
 
     private float oval_x;
     private float oval_y;
-
-    private float rec_x;
-    private float rec_y;
+    private DIRECTION oval_direction = DIRECTION.RIGHT;
 
     private float cir_x;
     private float cir_y;
+    private DIRECTION cir_direction = DIRECTION.DOWN;
 
     private float speed;
 
@@ -23,22 +25,49 @@ public class Rectangles extends BasicGame {
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-        this.sq_x = 100;
+        this.sq_x = 50;
+        this.sq_y = 50;
+
+        this.oval_x = 50;
+        this.oval_y = 50;
+
+        this.cir_x = 50;
+        this.cir_y = 50;
+
         this.speed = 2.0f;
+
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) throws SlickException {
-        this.rec_x = 50;
-        this.rec_y = 50;
-        this.rec_x += (float) delta / this.speed;
 
-        if (this.oval_x < 500) {
-            this.oval_x += (float) delta / this.speed;
-        } else if (this.oval_x > 500) {
-            this.oval_x += (float) delta / this.speed; }
+    if (sq_direction == DIRECTION.RIGHT) {
+        this.sq_x += (float) delta/speed;
+        if (this.sq_x > 600) {
+            this.sq_direction = DIRECTION.DOWN;
+        }
+    }
 
-        this.cir_y += (float) delta / this.speed;
+        if (sq_direction == DIRECTION.DOWN) {
+            this.sq_x += (float) delta/speed;
+            if (this.sq_y > 400) {
+                this.sq_direction = DIRECTION.LEFT;
+            }
+        }
+
+        if (sq_direction == DIRECTION.LEFT) {
+            this.sq_x += (float) delta/speed;
+            if (this.sq_y > 100) {
+                this.sq_direction = DIRECTION.UP;
+            }
+        }
+
+        if (sq_direction == DIRECTION.LEFT) {
+            this.sq_x += (float) delta/speed;
+            if (this.sq_x > 400) {
+                this.sq_direction = DIRECTION.UP;
+            }
+        }
 
         /* this.x++;
         this.y++;
@@ -48,10 +77,11 @@ public class Rectangles extends BasicGame {
         } */
     }
 
+
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
         graphics.drawString("Hello you!", 50, 50);
-        graphics.drawRect(this.rec_x, this.rec_y, 100, 100);
+        graphics.drawRect(this.sq_x, this.sq_y, 100, 100);
         graphics.drawOval(this.oval_x, this.oval_y, 100, 50);
         graphics.drawOval(this.cir_x, this.cir_y, 100, 100);
     }
