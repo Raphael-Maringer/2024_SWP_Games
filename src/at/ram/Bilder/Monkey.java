eb57283f890eccce18670a4a50c159fc5acd384a
Raphael <raphael.maringer@student.htldornbirn.at>
2024-05-29 08:42:43 +0200

    finished 09-Bilder

--- src/at/ram/Bilder/Monkey.java
+++ src/at/ram/Bilder/Monkey.java
@@ -6,17 +6,22 @@ import org.newdawn.slick.SlickException;
 
 public class Monkey implements Actor {
     private Image monkeyimage;
+    private float x,y;
 
-    public Monkey() {
+    public Monkey() throws SlickException {
+        Image tmp = new Image("at/ram/Bilder/monkey.png");
+        this.monkeyimage = tmp.getScaledCopy(200,100);
+        this.x = 100;
+        this.y = 100;
     }
 
     @Override
     public void render(Graphics graphics) throws SlickException {
-        this.monkeyimage = new Image("Bilder/monkey.png");
+        monkeyimage.draw(this.x,this.y);
     }
 
     @Override
     public void update(int delta) {
-        monkeyimage.draw(100,100);
+        this.x++;
     }
 }
--- src/at/ram/Bilder/ObjectsGame.java
+++ src/at/ram/Bilder/ObjectsGame.java
@@ -1,24 +1,46 @@
 package at.ram.Bilder;
 
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ObjectsGame extends BasicGame {
+    private List<Actor> actors;
+
+    public ObjectsGame(String title) {
+        super(title);
+    }
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-
+        this.actors = new ArrayList<>();
+        Monkey monkey = new Monkey();
+        this.actors.add(monkey);
     }
 
     @Override
-    public void update(GameContainer gameContainer, int i) throws SlickException {
-
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
+        for (Actor actor : this.actors) {
+            actor.update(delta);
+        }
     }
+
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        for (Actor actor : actors) {
+            actor.render(graphics);
+        }
+    }
 
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new at.ram.Bilder.ObjectsGame("Monkey"));
+            container.setDisplayMode(800, 600, false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
     }
 }
