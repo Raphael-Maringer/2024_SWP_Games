bd711c5bfe83a1c0c00bf47e6be0757a30c3d931
Raphael <raphael.maringer@student.htldornbirn.at>
2024-06-12 09:28:15 +0200

    working on project Blackjack: Flipping cards working

--- src/at/ram/Projekt/actors/Card.java
+++ src/at/ram/Projekt/actors/Card.java
@@ -6,23 +6,43 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class Card implements Actor{
-    private Image top;
+    private Image front;
     private Image back;
+    private Image currentSide;
+    private boolean isFront;
     private int points;
+    private float x,y;
 
-    public Card(Image top, Image back) {
-        this.top = top;
+    public Card(Image front, Image back, int points) {
+        this.front = front;
         this.back = back;
+        this.currentSide = back;
         this.points = points;
+        this.isFront = false;
+        this.x = 300;
+        this.y = 250;
     }
+
+    public void flip() {
+        if (this.currentSide == this.front) {
+            this.currentSide = this.back;
+        } else {
+            this.currentSide = this.front;
+        }
+    }
+
 
     @Override
     public void render(Graphics graphics) throws SlickException {
-        this.top.draw();
+        this.currentSide.draw(this.x, this.y);
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) {
 
+    }
+
+    public int getPoints() {
+        return points;
     }
 }
